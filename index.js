ec41edafd011917858f75f2c219f866edcdc5c6c
Joseph <frasier.joseph@gmail.com>
2023-05-16 15:01:32 -0500

    index

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const app = express();
 app.use(express.json());
 
 // Create an empty array to store restaurant data
-const restaurants = [];
+const restaurants = [{ name: "Uchi", style: "Asian", price: "$$$$" }];
 
 // Routes go here
 
+app.delete("/delete_restaurant/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  res.send("this is the id");
+});
 // Start the server
-app.listen(3000, () => {
+app.listen(3003, () => {
   console.log("Server is running on port 3000");
 });
