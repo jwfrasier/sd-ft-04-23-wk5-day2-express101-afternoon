b8fb33e9572814fec945e6329bdb637a1a0eb07d
Joseph <frasier.joseph@gmail.com>
2023-05-17 09:30:19 -0500

    answer for afternoon exercise

--- index.js
+++ index.js
@@ -10,10 +10,39 @@ let restaurants = [
 
 // Routes go here
 
+// view all restaurants
+app.get("/view_restaurants", (req, res) => {
+  res.json(restaurants);
+});
+// create a restaurant
 app.post("/create_restaurant", (req, res) => {
-  restaurants.push({ name: "Taco Bell", stle: "fast food", price: "$$" });
+  restaurants.push(req.body);
+  res.send(`Added new restaurant called ${req.body.name}`);
 });
 
+app.put("/update_restaurant", (req, res) => {
+  const indexOfRestaurantToUpdate = restaurants.findIndex(
+    (restaurant) => restaurant.name === req.body.name
+  );
+  if (indexOfRestaurantToUpdate === -1) {
+    res
+      .status(404)
+      .send("That restaurant does not exist, please consider adding it!");
+  } else {
+    if (req.body.name && req.body.price && req.body.style) {
+      restaurants[indexOfRestaurantToUpdate].name = req.body.nameToUpdateTo;
+      restaurants[indexOfRestaurantToUpdate].price = req.body.price;
+      restaurants[indexOfRestaurantToUpdate].style = req.body.style;
+      res.send(
+        `This is your newly updated restaurant ${restaurants[indexOfRestaurantToUpdate].name}`
+      );
+    } else {
+      res.send("You need to provide a name and a price and a style.");
+    }
+  }
+});
+
+// delete a restaurant
 app.post("/delete_restaurant", (req, res) => {
   const filteredRestaurants = restaurants.filter(
     (resta) => resta.name !== req.body.name
